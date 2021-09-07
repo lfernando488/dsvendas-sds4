2f7554e3d00e888d7553c383d786ba5544fd8d39
Luiz Fernando Geraldo dos Santos <lfernando488@gmail.com>
2021-09-06 23:55:26 -0300

    Creating project dsvendas

---
+++ backend/src/test/java/com/lfernando488/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lfernando488.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
