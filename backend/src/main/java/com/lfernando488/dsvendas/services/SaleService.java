81fe3b8effdde78fc6599f6e8cddf2d96213dd3e
Luiz Fernando Geraldo dos Santos <lfernando488@gmail.com>
2021-09-12 15:00:42 -0300

    Group by search

--- backend/src/main/java/com/lfernando488/dsvendas/services/SaleService.java
+++ backend/src/main/java/com/lfernando488/dsvendas/services/SaleService.java
@@ -1,5 +1,7 @@
 package com.lfernando488.dsvendas.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.lfernando488.dsvendas.dto.SaleDTO;
+import com.lfernando488.dsvendas.dto.SaleSuccessDTO;
+import com.lfernando488.dsvendas.dto.SaleSumDTO;
 import com.lfernando488.dsvendas.entities.Sale;
 import com.lfernando488.dsvendas.repositories.SaleRepository;
 import com.lfernando488.dsvendas.repositories.SellerRepository;
@@ -26,6 +30,16 @@ public class SaleService {
 		sellerRepo.findAll();
 		Page<Sale> result = saleRepo.findAll(pageable);
 		return result.map(item -> new SaleDTO(item));
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSumDTO> amountGroupedBySeller(){
+		return saleRepo.amountGroupedBySeller();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSuccessDTO> successGroupedBySeller(){
+		return saleRepo.successGroupedBySeller();
 	}
 	
 }
